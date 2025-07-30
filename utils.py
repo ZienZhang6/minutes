97cb9b0ce67bea7051b06d2d02a3c0b652e4d69c
enen <zhangzien1762062129@gmail.com>
2025-07-30 14:23:04 +0800

    Revert "fix:"
    This reverts commit b7893688124737636c34e932c566c15308a7bd8b.

--- utils.py
+++ utils.py
@@ -6,8 +6,6 @@ from pathlib import Path
 
 def convert_to_wav(in_filename: str) -> str:
     """Convert the input audio file to a wave file"""
-    in_filename = str(Path(in_filename))  # Ensure the path is a string
-    
     # 将win路径
     base_dir = Path(in_filename)
     # print(base_dir.parent)
@@ -18,7 +16,7 @@ def convert_to_wav(in_filename: str) -> str:
     out_filename = base_dir.parent / (base_dir.stem + ".wav")
 
     try:
-        _ = os.system(f'ffmpeg -y -i "{in_filename}" -acodec pcm_s16le -ac 1 -ar 16000 "{out_filename}"')
+        _ = os.system(f"ffmpeg -y -i '{str(base_dir.parent / base_dir.name)}' -acodec pcm_s16le -ac 1 -ar 16000 '{out_filename}'")
     except:
         print(f"Error converting {in_filename} to wav")
         return None
@@ -50,6 +48,6 @@ def chunk_strings(input_list: List[str], output_chunk_length: int) -> List[str]:
 
 
 if __name__ == '__main__':
-    in_filename = r'E:\learning\minutes\downloads\test_audios\e114.mp3'
+    in_filename = r'E:\learning\minutes-main\downloads\test_audios\e114.mp3'
 
     speech = convert_to_wav(in_filename)
