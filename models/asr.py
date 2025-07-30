05c57c3dc9d14dc7fe26761cfc2f49f41ca7abdf
enen <zhangzien1762062129@gmail.com>
2025-07-30 10:22:01 +0800

    Revert "feat："
    This reverts commit 7a26c100c3a6cffd7dd4ca67d9d65146051dd32c.

--- models/asr.py
+++ models/asr.py
@@ -122,15 +122,6 @@ def transcribe_with_vad(recognizer, vad_model, wav: np.ndarray, sample_rate: int
         timestamp['end_time'] = process_time(int(timestamp['end']/sample_rate*1000))
         results_list.append(timestamp)
         print(timestamp)
-
-        # 写入 TXT 文件
-        with open('results.txt', 'w', encoding='utf-8') as f:
-            for item in results_list:
-                f.write(f"ID: {item['id']}\n")
-                f.write(f"开始时间: {item['start_time']}\n")
-                f.write(f"结束时间: {item['end_time']}\n")
-                f.write(f"内容: {item['s']}\n")
-                f.write("-" * 50 + "\n")
     return results_list
 
 def main():
